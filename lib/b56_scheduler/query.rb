f9016420d99372cfc2739e1423eb8618ab68e359
Jacob Atzen <jacob@incremental.dk>
2012-03-16 15:34:53 +0100

    Add query object

--- lib/b56_scheduler/query.rb
+++ lib/b56_scheduler/query.rb
@@ -6,14 +6,36 @@ module B56Scheduler
     def initialize
       @included_events = []
       @excluded_events = []
+      @queries = []
     end
 
     def includes_event(event, opts = {})
       @included_events << event
+      @queries << { :includes => event }.merge(opts)
     end
 
     def excludes_event(event, opts = {})
       @excluded_events << event
+      @queries << { :excludes  => event }.merge(opts)
+    end
+
+    def match?(events)
+      @queries.all?{|query|
+        result = true
+        if(query[:includes])
+          event = events.detect{|event| event.name == query[:includes]}
+          if(query[:before])
+            result = !event.nil? && event.created_at < query[:before]
+          else
+            result = !event.nil?
+          end
+        end
+        if(query[:excludes])
+          event = events.detect{|event| event.name == query[:excludes]}
+          result = event.nil?
+        end
+        result
+      }
     end
   end
 end

646f87e2e6fbaa2962c292164c5800cb4a25987c
Jacob Atzen <jacob@incremental.dk>
2012-03-30 12:09:31 +0200

    Remove dead code

--- lib/b56_scheduler/event_repository.rb
+++ lib/b56_scheduler/event_repository.rb
@@ -15,25 +15,5 @@ module B56Scheduler
         query.match?(events)
       end.map{|array| array[0]}
     end
-
-    # def search2(query)
-    #   with_events = query.included_events
-    #   participants_for_events = with_events.map{ |with_event|
-    #     @events[with_event]
-    #   }
-
-    #   without_events = query.excluded_events
-    #   participants_excluded = without_events.map{ |without_event|
-    #     @events[without_event]
-    #   }
-
-    #   excluded = participants_excluded.inject{ |excluded_participants, excluded_participants_for_event|
-    #     excluded_participants & excluded_participants_for_event
-    #   } || []
-
-    #   participants_for_events.inject{ |selected_participants, participants_for_event|
-    #     selected_participants & participants_for_event
-    #   } - excluded
-    # end
   end
 end

9c2b3593cce391a2718d8699e7e529826f0e034c
Jacob Atzen <jacob@incremental.dk>
2012-03-30 12:10:36 +0200

    Remove unused ivar

--- lib/b56_scheduler/event_repository.rb
+++ lib/b56_scheduler/event_repository.rb
@@ -1,12 +1,10 @@
 module B56Scheduler
   class EventRepository
     def initialize
-      @events = Hash.new{|hash, key| hash[key] = []}
       @participants = Hash.new{|hash, key| hash[key] = []}
     end
 
     def notify(participant_id, event)
-      @events[event] << participant_id
       @participants[participant_id] << event
     end
 

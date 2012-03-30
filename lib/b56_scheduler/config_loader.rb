b3dac6d809ff819fe84f8b40d1c8f50340784825
Jacob Atzen <jacob@incremental.dk>
2012-03-30 14:48:23 +0200

    Add timed config loading

--- lib/b56_scheduler/config_loader.rb
+++ lib/b56_scheduler/config_loader.rb
@@ -9,8 +9,31 @@ module B56Scheduler
 
     def load_config(config)
       config['triggers'].each do |name, trigger_config|
+        if trigger_config['offset']
+          trigger_config['offset'] = parse_time(trigger_config['offset'])
+        end
         @schedule.add_trigger(name, trigger_config)
       end
+    end
+
+    def parse_time(time_string)
+      md = time_string.match(/([0-9]+)\s+([a-z]+)/)
+      count = md[1]
+      unit = md[2]
+      unit_to_i(unit) * count.to_i
+    end
+
+    def unit_to_i(unit)
+    case unit
+    when /month/
+      30 * 24 * 60 * 60
+    when /day/
+      24 * 60 * 60
+    when /hour/
+      60 * 60
+    when /minute/
+      60
+    end
     end
   end
 end
--- spec/config_loader_spec.rb
+++ spec/config_loader_spec.rb
@@ -14,4 +14,46 @@ describe B56Scheduler::ConfigLoader do
     )
     B56Scheduler::ConfigLoader.new(schedule).load_config(config)
   end
+
+  context "with a timed config" do
+    let(:config) { YAML::load_file('spec/fixtures/timed_config.yaml') }
+    before(:each) { schedule.stub(:add_trigger) }
+
+    it "loads the invitation" do
+      schedule.should_receive(:add_trigger).with(
+        'invitation',
+        {
+          'event'  => 'participant_join',
+          'action' => 'send_invite',
+          'offset' => 3600
+        }
+      )
+      B56Scheduler::ConfigLoader.new(schedule).load_config(config)
+    end
+
+    it "loads the reminder" do
+      schedule.should_receive(:add_trigger).with(
+        'reminder',
+        {
+          'event'  => 'participant_join',
+          'action' => 'send_invite',
+          'offset' => 2 * 24 * 60 * 60
+        }
+      )
+      B56Scheduler::ConfigLoader.new(schedule).load_config(config)
+    end
+
+    it "loads the final" do
+      schedule.should_receive(:add_trigger).with(
+        'final',
+        {
+          'event'  => 'participant_join',
+          'action' => 'send_invite',
+          'offset' => 2 * 30 * 24 * 60 * 60
+        }
+      )
+      B56Scheduler::ConfigLoader.new(schedule).load_config(config)
+    end
+  end
+
 end

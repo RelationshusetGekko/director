a565a10ca17ff94eb2cd445d60679fc80100d94c
Jacob Atzen <jacob@incremental.dk>
2012-03-30 12:14:13 +0200

    Remove unused ivars

--- lib/b56_scheduler/query.rb
+++ lib/b56_scheduler/query.rb
@@ -1,26 +1,19 @@
 module B56Scheduler
   class Query
-    attr_reader :included_events
-    attr_reader :excluded_events
-
     def initialize
-      @included_events = []
-      @excluded_events = []
-      @queries = []
+      @criteria = []
     end
 
     def includes_event(event, opts = {})
-      @included_events << event
-      @queries << { :includes => event }.merge(opts)
+      @criteria << { :includes => event }.merge(opts)
     end
 
     def excludes_event(event, opts = {})
-      @excluded_events << event
-      @queries << { :excludes  => event }.merge(opts)
+      @criteria << { :excludes  => event }.merge(opts)
     end
 
     def match?(events)
-      @queries.all?{|query|
+      @criteria.all?{|query|
         result = true
         if(query[:includes])
           event = events.detect{|event| event.name == query[:includes]}

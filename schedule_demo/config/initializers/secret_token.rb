6f51809b52ae29f51cd8343c7772f4fdcd9f102e
Jacob Atzen <jacob@incremental.dk>
2012-03-30 15:04:54 +0200

    Add demo app

---
+++ schedule_demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScheduleDemo::Application.config.secret_token = '5565a3fe0c582e33702c1b6010ad6b02f9f7298663910c744ad163506b4954a4b2392fa3119c188ffdba050ad360249cbeec91e2d69165f477d0344468145e26'
